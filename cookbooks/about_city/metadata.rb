fc3120757bd73fee20bd79c362705948868a665a
Curt Wilhelm <curt@rightscale.com>
2012-06-12 17:23:48 -0600

    update meta

--- cookbooks/about_city/metadata.rb
+++ cookbooks/about_city/metadata.rb
@@ -1,5 +1,5 @@
 maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer_email "curt@rightscale.com"
 license          "All rights reserved"
 description      "Installs/Configures about_city"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))

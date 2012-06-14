15b780a35d35668b39cb971262a6d117869d5c3b
Curt Wilhelm <curt@rightscale.com>
2012-06-14 16:19:52 -0600

    add setup ssh

--- cookbooks/hadoop/recipes/do_hbase_config.rb
+++ cookbooks/hadoop/recipes/do_hbase_config.rb
@@ -1,26 +1,18 @@
 rs_utils_marker :begin
 
 include_recipe "hadoop::default"
-log "Installing hadoop hadoop-env.sh to #{node[:hadoop][:install_dir]}/conf"
-template "#{node[:hadoop][:install_dir]}/conf/hadoop-env.sh" do
-  source "hadoop-env.sh.erb"
+
+log "Installing hbase hbase-env.sh to #{node[:hbase][:install_dir]}/conf"
+template "#{node[:hbase][:install_dir]}/conf/hbase-env.sh" do
+  source "hbase-env.sh.erb"
 #  owner "#{node[:tomcat][:app_user]}"
 #  group "#{node[:tomcat][:app_user]}"
   mode "0644"
 end
 
-log "Installing hadoop core-site.xml to #{node[:hadoop][:install_dir]}/conf"
-template "#{node[:hadoop][:install_dir]}/conf/core-site.xml" do
-  source "core-site.xml.erb"
-#  owner "#{node[:tomcat][:app_user]}"
-#  group "#{node[:tomcat][:app_user]}"
-  mode "0644"
-
-end
-
-log "Installing hadoop hdfs-site.xml to #{node[:hadoop][:install_dir]}/conf"
-template "#{node[:hadoop][:install_dir]}/conf/hdfs-site.xml" do
-  source "hdfs-site.xml.erb"
+log "Installing hbase hbase-site.xml to #{node[:hbase][:install_dir]}/conf"
+template "#{node[:hbase][:install_dir]}/conf/hbase-site.xml" do
+  source "hbase-site.xml.erb"
 #  owner "#{node[:tomcat][:app_user]}"
 #  group "#{node[:tomcat][:app_user]}"
   mode "0644"

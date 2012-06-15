1fa6934e36b14bf2d33897fc8ab8419d6b78bf73
Curt Wilhelm <curt@rightscale.com>
2012-06-15 09:48:44 -0600

    hbase init

--- cookbooks/hadoop/recipes/do_hbase_init.rb
+++ cookbooks/hadoop/recipes/do_hbase_init.rb
@@ -1,6 +1,11 @@
 rs_utils_marker :begin
 
-  
+  log "  Running start sequence"
+execute "create hbase hdfs dir" do
+  command "#{node[:hbase][:install_dir]}/bin/start-hbase.sh"
+  action :run
+end
+
 log "  Running start sequence"
 execute "start hadoop" do
   command "#{node[:hbase][:install_dir]}/bin/start-hbase.sh"

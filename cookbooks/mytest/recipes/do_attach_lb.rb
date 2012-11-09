78338cc7ed78e00f0547b87bb9fe179a6a41cc25
Curt Wilhelm <curt@rightscale.com>
2012-11-09 13:55:08 -0700

    add resource

--- cookbooks/mytest/recipes/do_attach_lb.rb
+++ cookbooks/mytest/recipes/do_attach_lb.rb
@@ -1,6 +1,6 @@
 
 log "  Sending remote attach request..."
-lb pool_name do
+lb "mytest" do
   backend_id node[:rightscale][:instance_uuid]
   backend_ip node[:app][:ip]
   backend_port 8080 # <- virtual host port in apache for tilecache from input.

8d089342d34ca6fcb05dd4c8f54127b88dc43f74
Curt Wilhelm <curt@rightscale.com>
2012-11-14 11:50:13 -0700

    change pool name

--- cookbooks/mytest/recipes/do_attach_lb.rb
+++ cookbooks/mytest/recipes/do_attach_lb.rb
@@ -2,7 +2,7 @@
 pool_name="/mytest"
 
 sys_firewall "Open this appserver's ports to all loadbalancers" do
-  machine_tag "loadbalancer:#{pool_name}=lb"
+  machine_tag "loadbalancer:#{pool_name.gsub("\/","_")}=lb"
   port 8080
   enable true
   action :update
@@ -14,7 +14,7 @@ node[:lb][:service][:provider] = "lb_haproxy"
 #lb_tag pool_name
 
 log "  Sending remote attach request..."
-lb pool_name do
+lb pool_name.gsub("\/","_") do
   backend_id node[:rightscale][:instance_uuid]
   backend_ip node[:app][:ip]
   backend_port 8080 # <- virtual host port in apache for tilecache from input.
